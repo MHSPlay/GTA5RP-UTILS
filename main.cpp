4cb6b66e8c09f9758ad774c3ec06968b54fd92f2
MHSPlay <41786546+MHSPlay@users.noreply.github.com>
2024-11-08 19:05:04 +1000

    GTA5RP C++ BOTS

---
+++ main.cpp
@@ -0,0 +1,242 @@
+﻿#include "src/includes.hpp"
+
+void cursor() 
+{
+    POINT p; // Структура для хранения координат
+
+    while (true) {
+        // Получаем текущие координаты курсора
+        if (GetCursorPos(&p)) {
+            std::cout << "X: " << p.x << ", Y: " << p.y << std::endl;
+        }
+
+        // Задержка, чтобы избежать перегрузки консоли
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        system("cls");
+    }
+}
+
+int main( ) {
+    shadowcall< BOOL >( "SetConsoleTitleA", ( utils::randomLowerString( utils::randomInt( 16, 28 ) ).c_str( ) ) );
+
+    //cursor();
+
+    std::cout << "[?] Important! This program is not a cheat or anything else\nIt just simulates pressing keys on your keyboard via WinAPI.\n!!!USE IT AT YOUR OWN RISK!!! \nThe author is not responsible for your account!" << std::endl;
+
+    std::this_thread::sleep_for( std::chrono::seconds( 5 ) );
+
+    std::cout << "[~] Open GTA5." << std::endl;
+
+    // init
+    bool once = false;
+    while ( !once ) {
+        DWORD ForegroundWindowProcessID;
+        GetWindowThreadProcessId( GetForegroundWindow( ), &ForegroundWindowProcessID );
+        if ( utils::GetProcessId( "GTA5.exe" ) == ForegroundWindowProcessID ) {
+            utils::window = GetForegroundWindow( );
+            std::cout << "[+] GTA5RP.exe has been found!" << std::endl;
+            once = true;
+        }
+    }
+
+	int selectType = 0;
+    int resolution_type = 1;
+    std::cout << "Select:\n 1. AntiAFK\n 2. Jail(sewing)(soon)\n 3. Fast loot(soon)\n 4. EMS fast revive(soon)\n 5. Harbor bot\ntype: ";
+	std::cin >> selectType;
+
+	switch ( selectType ) {
+		case 1: {
+            // clear
+            system( "cls" );
+
+            // notify
+            std::cout << "anti-afk started.\n";
+
+            // sleep
+            std::this_thread::sleep_for( std::chrono::seconds( 2 ) );
+
+            // activate window
+            utils::ActivateWindow( );
+
+            restart:
+            auto antiAFK = [ & ]( ) {
+
+                auto start = std::chrono::steady_clock::now( );
+                while ( true ) {
+                    
+                    // activate window
+                    utils::ActivateWindow( );
+
+                    auto now = std::chrono::steady_clock::now( );
+                    auto elapsed = std::chrono::duration_cast< std::chrono::minutes >( now - start );
+
+                    if ( elapsed.count( ) >= 30 ) {
+                        std::cout << "[!] 30 minutes have passed." << std::endl;
+                        break;
+                    }
+
+                    utils::shadowSendKeyCode( 0x11, true );
+                    utils::shadowSendKeyCode( 0x1F, true );
+
+                    std::this_thread::sleep_for( std::chrono::seconds( 5 ) );
+                }
+
+            };
+
+			auto fortuneWheel = [ & ] ( int resolution = 1 ) {
+       
+                // activate window
+                utils::ActivateWindow( );
+
+                std::pair< int, int > coordinates[] = {
+                        { 1645, 905 },
+                        { 760, 650 },
+                        { 960, 885 },
+                };
+
+                while ( true ) {
+                    // open phone
+                    utils::shadowSendKey( VK_UP );
+                    std::cout << "[+] open the phone." << std::endl;
+
+                    for (const auto& coord : coordinates) {
+                        utils::shadowClickMouseAt( coord.first, coord.second );
+                        std::cout << "[+] clicked at (" << coord.first << ", " << coord.second << ")" << std::endl;
+                        std::this_thread::sleep_for( std::chrono::seconds( 1 ) );
+                    }
+
+                    std::this_thread::sleep_for( std::chrono::minutes( 1 ) );
+
+                    for ( int i = 0; i < 2; ++i ) {
+                        utils::shadowSendKey( VK_ESCAPE );
+                        std::cout << "[+] ESC pressed." << std::endl;
+                    }
+                    utils::shadowSendKey( VK_BACK );
+                    std::cout << "[+] BACKSPACE pressed." << std::endl;
+                }
+
+            };
+
+            antiAFK( );
+            fortuneWheel( );
+            goto restart;
+		}
+		break;
+
+        case 2: {
+            system( "cls" );
+
+            std::cout << "Press F5 to start." << std::endl;
+
+            std::this_thread::sleep_for( std::chrono::seconds( 1 ) );
+
+            // activate window
+            utils::ActivateWindow(  );
+
+            auto getPixel = [ & ]( ) {
+
+                bool isActive = false;
+                bool lastState = !isActive;
+
+                while ( true ) {
+
+                    if ( GetAsyncKeyState( VK_F5 ) & 1 )
+                        isActive = !isActive;
+
+                    if ( isActive != lastState ) {
+                        system( "cls" );
+
+                        std::cout << "Press F5 to start." << std::endl;
+
+                        if ( isActive ) {
+                            std::cout << "Sewing started." << std::endl;
+
+
+
+
+
+
+                            isActive = false;
+                        }
+                        
+                        lastState = isActive;
+                    }
+
+                    // save CPU
+                    std::this_thread::sleep_for( std::chrono::milliseconds( 125 ) );
+                }
+            };
+
+            getPixel( );
+
+        }
+        break;
+
+        case 5: {
+            system( "cls" );
+
+            std::cout << "Press F5 to start, F5 to stop." << std::endl;
+
+            // activate window
+            utils::ActivateWindow( );
+
+            auto getPixel = [ & ]( ) {
+
+                bool isActive = false;
+                bool lastState = !isActive;
+
+                while ( true ) {
+
+                    if ( GetAsyncKeyState( VK_F5 ) & 1 )
+                        isActive = !isActive;
+
+                    if ( isActive != lastState ) {
+                        system( "cls" );
+
+                        std::cout << "Press F5 to start, F5 to stop." << std::endl;
+
+                        if ( isActive )
+                        {
+                            std::cout << "Harbor Started." << std::endl;
+                            utils::shadowSendKeyCode( 0x11, true );
+                            utils::shadowSendKeyCode( 0x2A, true );
+                        }
+                        else 
+                        {
+                            std::cout << "Harbor Paused." << std::endl;
+                            utils::shadowSendKeyCode( 0x11, false );
+                            utils::shadowSendKeyCode( 0x2A, false );
+                        }
+
+                        lastState = isActive;
+                    }
+
+                    if ( utils::PixelSearch( RGB( 126, 211, 33 ), 962, 476, 970, 510, 0 ) )
+                        utils::shadowSendKey( VK_E );
+
+                    // save CPU
+                    std::this_thread::sleep_for( std::chrono::milliseconds( 25 ) );
+                }
+            };
+
+            getPixel( );
+
+        }
+        break;
+
+
+	default:
+		break;
+	}
+
+	/*
+	*	run your overlay here instead of the while true loop
+	*/
+
+    while (true)
+    {
+
+    }
+
+	return EXIT_SUCCESS;
+}
---
+++ src/utils/utils.hpp
@@ -0,0 +1,305 @@
+﻿#pragma once
+
+namespace utils
+{
+    HWND window;
+
+    struct Point {
+        int x;
+        int y;
+    };
+
+	inline std::string random_str(size_t length)
+	{
+		auto randchar = []() -> char
+		{
+			const char charset[] =
+			{
+				"0123456789"
+				"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+				"abcdefghijklmnopqrstuvwxyz"
+			};
+
+			const size_t max_index = (sizeof(charset) - 1);
+			return charset[rand() % max_index];
+		};
+		std::string str(length, 0);
+		std::generate_n(str.begin(), length, randchar);
+		return str;
+	}
+
+	inline std::string randomLowerString( size_t length ){
+		auto randchar = [ ]( ) -> char {
+			const char charset[ ] = {
+				"0123456789"
+				"abcdefghijklmnopqrstuvwxyz"
+			};
+
+			const size_t max_index = ( sizeof( charset ) - 1 );
+			return charset[ rand( ) % max_index ];
+		};
+		std::string str( length, 0 );
+		std::generate_n( str.begin( ), length, randchar );
+		return str;
+	}
+
+	inline int randomInt( int min, int max ) {
+		static bool first = true;
+		if ( first ) {
+			srand( ( unsigned int )time( NULL ) );
+			first = false;
+		}
+		return min + rand( ) % ( ( max + 1 ) - min );
+	}
+
+    DWORD GetProcessId(LPCSTR ProcessName) {
+        PROCESSENTRY32 pt;
+        HANDLE hsnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+        pt.dwSize = sizeof(PROCESSENTRY32);
+        if (Process32First(hsnap, &pt)) {
+            do {
+                if (!lstrcmpi(pt.szExeFile, ProcessName)) {
+                    CloseHandle(hsnap);
+                    return pt.th32ProcessID;
+                }
+            } while (Process32Next(hsnap, &pt));
+        }
+        CloseHandle(hsnap);
+        return 0;
+    }
+
+	inline bool ActivateWindow( ) {
+
+		if ( !window )
+			return false;
+
+		SetForegroundWindow( window );
+		return true;
+	}
+
+	inline void shadowSendKey( WORD virtualKey ) {
+		INPUT input = { 0 };
+		input.type = INPUT_KEYBOARD;
+		input.ki.wVk = virtualKey;
+
+		shadowcall< UINT >( "SendInput", 1, &input, sizeof( INPUT ) );
+		input.ki.dwFlags = KEYEVENTF_KEYUP;
+		shadowcall< UINT >( "SendInput", 1, &input, sizeof( INPUT ) );
+	}
+
+    inline void shadowSendKeyCode( WORD virtualKey, bool hold = false ) {
+        INPUT input = { 0 };
+
+        input.type = INPUT_KEYBOARD;
+        input.ki.time = 0;
+        input.ki.wVk = 0;
+        input.ki.dwExtraInfo = 0;
+        input.ki.dwFlags = KEYEVENTF_SCANCODE;
+        input.ki.wScan = virtualKey;
+
+        shadowcall< UINT >( "SendInput", 1, &input, sizeof( INPUT ) );
+
+        if ( !hold ) {
+            input.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
+            shadowcall< UINT >( "SendInput", 1, &input, sizeof( INPUT ) );
+        }
+    }
+
+	inline void shadowClickMouseAt( int x, int y, bool doubleClick = false ) {
+		shadowcall< BOOL >( "SetCursorPos", x, y );
+
+		INPUT input = { 0 };
+		input.type = INPUT_MOUSE;
+		input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+		shadowcall< UINT >( "SendInput", 1, &input, sizeof( INPUT ) );
+
+		input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+		shadowcall< UINT >( "SendInput", 1, &input, sizeof( INPUT ) );
+
+        if ( doubleClick ) {
+            input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+		    shadowcall< UINT >( "SendInput", 1, &input, sizeof( INPUT ) );
+
+		    input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+		    shadowcall< UINT >( "SendInput", 1, &input, sizeof( INPUT ) );
+        }
+	}
+
+    inline bool PixelSearch(COLORREF targetColor, int startX, int startY, int width, int height, int variation = 0) {
+        HDC hdcScreen = GetDC(NULL);
+        if (!hdcScreen)
+            return false;
+
+        HDC hdcMem = CreateCompatibleDC(hdcScreen);
+        if (!hdcMem) {
+            ReleaseDC(NULL, hdcScreen);
+            return false;
+        }
+
+        HBITMAP hBitmap = CreateCompatibleBitmap(hdcScreen, width, height);
+        if (!hBitmap) {
+            DeleteDC(hdcMem);
+            ReleaseDC(NULL, hdcScreen);
+            return false;
+        }
+
+        HGDIOBJ hOldObj = SelectObject(hdcMem, hBitmap);
+        if (!BitBlt(hdcMem, 0, 0, width, height, hdcScreen, startX, startY, SRCCOPY)) {
+            SelectObject(hdcMem, hOldObj);
+            DeleteObject(hBitmap);
+            DeleteDC(hdcMem);
+            ReleaseDC(NULL, hdcScreen);
+            return false;
+        }
+
+        BITMAPINFO bmpInfo = {};
+        bmpInfo.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+        bmpInfo.bmiHeader.biWidth = width;
+        bmpInfo.bmiHeader.biHeight = -height; // Negative to indicate top-down bitmap
+        bmpInfo.bmiHeader.biPlanes = 1;
+        bmpInfo.bmiHeader.biBitCount = 24;
+        bmpInfo.bmiHeader.biCompression = BI_RGB;
+
+        std::unique_ptr<BYTE[]> pixels(new BYTE[width * height * 3]);
+
+        if (!GetDIBits(hdcMem, hBitmap, 0, height, pixels.get(), &bmpInfo, DIB_RGB_COLORS)) {
+            SelectObject(hdcMem, hOldObj);
+            DeleteObject(hBitmap);
+            DeleteDC(hdcMem);
+            ReleaseDC(NULL, hdcScreen);
+            return false;
+        }
+
+        BYTE targetRed = GetRValue(targetColor);
+        BYTE targetGreen = GetGValue(targetColor);
+        BYTE targetBlue = GetBValue(targetColor);
+
+        bool found = false;
+        for (int y = 0; y < height && !found; ++y) {
+            for (int x = 0; x < width; ++x) {
+                int i = (y * width + x) * 3;
+
+                BYTE red = pixels[i + 2];    // Red is the third byte
+                BYTE green = pixels[i + 1];  // Green is the second byte
+                BYTE blue = pixels[i];       // Blue is the first byte
+
+                if (std::abs(targetRed - red) <= variation &&
+                    std::abs(targetGreen - green) <= variation &&
+                    std::abs(targetBlue - blue) <= variation) {
+                    found = true;
+                    break;
+                }
+            }
+        }
+
+        SelectObject(hdcMem, hOldObj);
+        DeleteObject(hBitmap);
+        DeleteDC(hdcMem);
+        ReleaseDC(NULL, hdcScreen);
+
+        return found;
+    }
+
+    inline bool ComparePixels(const BYTE* screenPixel, const BYTE* templatePixel, int variation) {
+        return (std::abs(screenPixel[0] - templatePixel[0]) <= variation &&
+            std::abs(screenPixel[1] - templatePixel[1]) <= variation &&
+            std::abs(screenPixel[2] - templatePixel[2]) <= variation);
+    }
+
+    inline bool CompareRegion(const BYTE* screenPixels, int screenWidth, int screenHeight,
+        const BYTE* templatePixels, int templateWidth, int templateHeight, int variation) {
+        for (int y = 0; y < templateHeight; ++y) {
+            for (int x = 0; x < templateWidth; ++x) {
+                int screenIndex = ((y * screenWidth) + x) * 3;
+                int templateIndex = ((y * templateWidth) + x) * 3;
+                if (!ComparePixels(&screenPixels[screenIndex], &templatePixels[templateIndex], variation))
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    inline Point ImageSearch(HBITMAP hTemplate, int startX, int startY, int searchWidth, int searchHeight, int variation = 0) {
+        HDC hdcScreen = GetDC(NULL);
+        if (!hdcScreen)
+            return { -1, -1 };
+
+        HDC hdcMem = CreateCompatibleDC(hdcScreen);
+        if (!hdcMem) {
+            ReleaseDC(NULL, hdcScreen);
+            return { -1, -1 };
+        }
+
+        HBITMAP hBitmap = CreateCompatibleBitmap(hdcScreen, searchWidth, searchHeight);
+        if (!hBitmap) {
+            DeleteDC(hdcMem);
+            ReleaseDC(NULL, hdcScreen);
+            return { -1, -1 };
+        }
+
+        HGDIOBJ hOldObj = SelectObject(hdcMem, hBitmap);
+        if (!BitBlt(hdcMem, 0, 0, searchWidth, searchHeight, hdcScreen, startX, startY, SRCCOPY)) {
+            SelectObject(hdcMem, hOldObj);
+            DeleteObject(hBitmap);
+            DeleteDC(hdcMem);
+            ReleaseDC(NULL, hdcScreen);
+            return { -1, -1 };
+        }
+
+        BITMAP templateInfo = {};
+        GetObject(hTemplate, sizeof(templateInfo), &templateInfo);
+
+        int templateWidth = templateInfo.bmWidth;
+        int templateHeight = templateInfo.bmHeight;
+        BITMAPINFO bmpInfo = {};
+        bmpInfo.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+        bmpInfo.bmiHeader.biWidth = searchWidth;
+        bmpInfo.bmiHeader.biHeight = -searchHeight;  // Top-down
+        bmpInfo.bmiHeader.biPlanes = 1;
+        bmpInfo.bmiHeader.biBitCount = 24;
+        bmpInfo.bmiHeader.biCompression = BI_RGB;
+
+        std::vector<BYTE> screenPixels(searchWidth * searchHeight * 3);
+        if (!GetDIBits(hdcMem, hBitmap, 0, searchHeight, screenPixels.data(), &bmpInfo, DIB_RGB_COLORS)) {
+            SelectObject(hdcMem, hOldObj);
+            DeleteObject(hBitmap);
+            DeleteDC(hdcMem);
+            ReleaseDC(NULL, hdcScreen);
+            return { -1, -1 };
+        }
+
+        bmpInfo.bmiHeader.biWidth = templateWidth;
+        bmpInfo.bmiHeader.biHeight = -templateHeight;  // Top-down
+        std::vector<BYTE> templatePixels(templateWidth * templateHeight * 3);
+        if (!GetDIBits(hdcScreen, hTemplate, 0, templateHeight, templatePixels.data(), &bmpInfo, DIB_RGB_COLORS)) {
+            SelectObject(hdcMem, hOldObj);
+            DeleteObject(hBitmap);
+            DeleteDC(hdcMem);
+            ReleaseDC(NULL, hdcScreen);
+            return { -1, -1 };
+        }
+
+        Point foundPosition = { -1, -1 };
+        for (int y = 0; y <= searchHeight - templateHeight; ++y) {
+            for (int x = 0; x <= searchWidth - templateWidth; ++x) {
+                int offset = (y * searchWidth + x) * 3;
+                if (CompareRegion(&screenPixels[offset], searchWidth, searchHeight,
+                    templatePixels.data(), templateWidth, templateHeight, variation)) {
+                    foundPosition = { startX + x, startY + y };
+                    break;
+                }
+            }
+            if (foundPosition.x != -1)
+                break;
+        }
+
+        SelectObject(hdcMem, hOldObj);
+        DeleteObject(hBitmap);
+        DeleteDC(hdcMem);
+        ReleaseDC(NULL, hdcScreen);
+
+        return foundPosition;
+    }
+
+
+}
